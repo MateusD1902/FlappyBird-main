3e3bf209f081513ea2fa1078988c8e0a973c8516
MateusD1902 <mateusd1902@gmail.com>
2024-03-18 21:42:55 -0300

    Posição Aleatória Dos Canos
    Posição Aleatória Dos Canos.

--- Assets/Scripts/PipesManager.cs
+++ Assets/Scripts/PipesManager.cs
@@ -28,7 +28,11 @@ public class PipesManager : MonoBehaviour
         }
         void CreatPipe()
         {
-            Instantiate(pipeModel, spawnPoint.position, Quaternion.identity);
+            var pipeGameObject = Instantiate(pipeModel, spawnPoint.position, Quaternion.identity);
+            var pipeTransform = pipeGameObject.GetComponent<Transform>();
+            float y = Random.Range(-1.6f, 0.26f);
+            pipeTransform.position = new Vector3(spawnPoint.position.x, y);
+            
         }
     }
 
